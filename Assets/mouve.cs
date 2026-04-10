7cbf36865e50bd3717c36bd706bcc91a1f7f2692
idle2211 <danielmamulat@gmail.com>
2026-04-10 16:42:41 +0200

    position update

--- Assets/mouve.cs
+++ Assets/mouve.cs
@@ -4,32 +4,43 @@ public class ChataigneMovement : MonoBehaviour
 {
     public float speed = 5f;
 
-    private float inputX = 0f;
-    private float inputY = 0f;
+    float inputX = 0f;
+    float inputY = 0f;
+
+    float lastMessageTime;
 
     void Update()
     {
-        // DEBUG
+        // STOP AUTOMATIQUE si plus de signal
+        if (Time.time - lastMessageTime > 0.2f)
+        {
+            inputX = 0f;
+            inputY = 0f;
+        }
+
         Debug.Log("X: " + inputX + " | Y: " + inputY);
 
-        // MOUVEMENT
-        Vector3 move = new Vector3(inputX, 0, inputY);
+        // MOUVEMENT 2D sur plan XY (comme tu veux)
+        Vector3 move = new Vector3(inputX, inputY, 0f);
+
         transform.Translate(move * speed * Time.deltaTime);
+    }
+
+    public void OnMoveX(float value)
+    {
+        inputX = Mathf.Clamp(value, -1f, 1f);
+        lastMessageTime = Time.time;
+    }
+
+    public void OnMoveY(float value)
+    {
+        inputY = Mathf.Clamp(value, -1f, 1f);
+        lastMessageTime = Time.time;
     }
 
     public void Move(Vector2 direction)
     {
         OnMoveX(direction.x);
         OnMoveY(direction.y);
-    }
-
-    public void OnMoveX(float value)
-    {
-        inputX = Mathf.Clamp(value, -1f, 1f);
-    }
-
-    public void OnMoveY(float value)
-    {
-        inputY = Mathf.Clamp(value, -1f, 1f);
     }
 }
